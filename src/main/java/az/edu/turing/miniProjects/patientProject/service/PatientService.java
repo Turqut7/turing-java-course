e88ec73a8794eb92e65037f594c96ab7fb154c2e
Turqut7 <turqutismayil007@gmail.com>
2024-10-13 23:56:50 +0400

    linkedlist and mergeSortedArrays done

--- src/main/java/az/edu/turing/miniProjects/patientProject/service/PatientService.java
+++
@@ -1,9 +0,0 @@
-package az.edu.turing.miniProjects.patientProject.service;
-
-import az.edu.turing.miniProjects.patientProject.model.dto.PatientDTO;
-
-public interface PatientService {
-    PatientDTO getPatientById(int id);
-
-    void registerPatient(PatientDTO patient);
-}
